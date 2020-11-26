16bb56592862ed0fc7ebe1b5151092693503d322
benibenisan <72596397+benibenisan@users.noreply.github.com>
2020-11-26 10:49:48 +0900

    aaaaa

--- src/javajava.java
+++ src/javajava.java
@@ -3,8 +3,8 @@
 public class javajava {
 
 	public static void main(String[] args) {
-		System.out.printf("稲\n田\n柊\n弥\n");
-
+		System.out.println("稲\n田\n柊\n弥\n");
+		System.out.println("aiueo");
 	}
 
 }

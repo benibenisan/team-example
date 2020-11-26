c2fb3b146f45a5d3e4170e3a10f793cae4449359
benibenisan <72596397+benibenisan@users.noreply.github.com>
2020-11-26 10:56:39 +0900

    aaaaaa

--- src/javajava.java
+++ src/javajava.java
@@ -5,6 +5,7 @@ public class javajava {
 	public static void main(String[] args) {
 		System.out.println("稲\n田\n柊\n弥\n");
 		System.out.println("aiueo");
+		System.out.println("1111jjjjj1111")
 	}
 
 }
